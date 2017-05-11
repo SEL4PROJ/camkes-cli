65b3227bbaeabd7022a45c12901306e2ce3031d8
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2017-05-11 16:30:22 +1000

    Switch to github url

--- defaults.py
+++ defaults.py
@@ -1,2 +1,2 @@
-CAMKES_MANIFEST_URL = "ssh://git@bitbucket.keg.ertos.in.nicta.com.au:7999/seL4/camkes-manifest.git"
+CAMKES_MANIFEST_URL = "https://github.com/seL4/camkes-manifest.git"
 CAMKES_MANIFEST_NAME = "next.xml"
